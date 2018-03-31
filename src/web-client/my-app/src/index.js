834a464ceea04f85716d99c41d769d83426e301f
Sherjeel Sikander <sherjeel.337@gmail.com>
2018-03-31 04:22:27 +0200

    add map js and css

--- src/web-client/my-app/src/index.js
+++ src/web-client/my-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './samples/App';
+import Map from './map/Map';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Map />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/web-client/my-app/src/map/Map.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Map.css';
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="Map">
+        <header className="Map-header">
+          <h1 className="Map-title">Tourist Route Recommender</h1>
+        </header>
+        <p className="Map-intro">
+          Add map here
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Map;
