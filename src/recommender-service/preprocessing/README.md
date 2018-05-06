b02250e12e98985211cb292cadd191d808656e15
Sherjeel Sikander <sherjeel.337@gmail.com>
2018-05-06 19:55:23 +0200

    Add timers to see time required for creating dictionary from file, serializing and deserializing.

--- src/recommender-service/preprocessing/README.md
+++ src/recommender-service/preprocessing/README.md
@@ -10,4 +10,17 @@ where munich_small.osm is your osm XML file. Note that the argument passed is re
 
 The script will create a .nodes and .ways file with same filename (e.g. munich_small.nodes and munich_small.ways) in the same directory where .osm file is present.
 
-# Step 3
+# Step 2
+Create a dictionary from nodes file (e.g. munich_small.nodes) and dump it (munich_small.nodes.searilize).
+
+Some stats
+
+File size (munich_large.nodes): 182 MB
+Time to read file and create dictionary: 83.20 sec
+Time to dump dictionary: 4.86 sec
+Time to read dumped dictionary: 2.18 sec
+
+File size (munich_small.nodes): 6.8 MB
+Time to read file and create dictionary: 3.28 sec
+Time to dump dictionary: 0.15 sec
+Time to read dumped dictionary: 0.08 sec
--- src/recommender-service/preprocessing/node_dictionary.py
+++ src/recommender-service/preprocessing/node_dictionary.py
@@ -1,6 +1,7 @@
 import sys
 import xml.etree.ElementTree as ET
 import pickle
+import time
 
 if len(sys.argv) > 2:
     print("Invalid argument list. Only 1 filename argument allowed.")
@@ -10,7 +11,7 @@ if len(sys.argv) == 2:
     filename = sys.argv[1]
     path = ''
 else:
-    filename = 'munich_large'
+    filename = 'munich_small'
     path = 'D:/Documents/Thesis/master-thesis/src/recommender-service/map/'
     
 map_nodes = path + filename + '.nodes'
@@ -18,14 +19,23 @@ map_nodes_serialize = path + filename + '.nodes.serialize'
 node_dict = {}
 
 try:
+    readfiletime = time.time()
     mapNodesFile = open(map_nodes, 'r', encoding="utf8")
     for line in mapNodesFile:    
         node = ET.fromstring(line)
         node_dict[node.attrib['id']] = (node.attrib['lat'], node.attrib['lon']) 
     mapNodesFile.close()
     
+    dumpfiletime = time.time()
+    print("Time to read file: %(time).2f sec" % {'time': (dumpfiletime - readfiletime)})
     pickle.dump(node_dict, open(map_nodes_serialize, "wb"))
+
+    loadfiletime = time.time()
+    print("Time to dump file: %(time).2f sec" % {'time': (loadfiletime - dumpfiletime)})
     node_dict_loaded = pickle.load(open(map_nodes_serialize, "rb"))
+
+    completiontime = time.time()
+    print("Time to read dumped file: %(time).2f sec" % {'time': (completiontime - loadfiletime)})
     print("Done")
 except IOError:
     print ("Could not read file or file does not exist: ", map)
