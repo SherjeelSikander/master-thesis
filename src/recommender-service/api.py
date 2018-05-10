be6a9d917fba913d93b770cfaf0831b6fcc958c9
Sherjeel Sikander <sherjeel.337@gmail.com>
2018-05-10 23:25:01 +0200

    create api service for finding nearest nodes and finding shortest path

--- src/recommender-service/api.py
+++ src/recommender-service/api.py
@@ -1,30 +1,11 @@
 #!src/recommender-service/api
 from flask import Flask, request
 from flask_cors import CORS
-import os
-import networkx as nx
-import pickle
-import sys
 import json
+import service
 
 app = Flask(__name__)
 CORS(app)
-graph = nx.Graph()
-nodes = {}
-
-def loadGraph():
-    
-    nodes_path = "D:/Documents/Thesis/master-thesis/src/recommender-service/map/munich_large.nodes.serialize"
-    global nodes 
-    nodes = pickle.load(open(nodes_path, "rb"))
-    print("Nodes Loaded")
-    
-    path = "D:/Documents/Thesis/master-thesis/src/recommender-service/map/munich_large.ways.serialize"
-    global graph 
-    graph = pickle.load(open(path, "rb"))
-    print("Graph Loaded")
-
-loadGraph()
 
 @app.route('/')
 def index():
@@ -37,9 +18,7 @@ def getRoute():
     destinationLat = request.args.get('destinationLat')
     destinationLng = request.args.get('destinationLng')
     
-    shortest_path = nx.shortest_path(graph, source='274547', target='3759741031')
-    for index, item in enumerate(shortest_path):
-        shortest_path[index] = nodes[item]
+    shortest_path = service.getShortestPath(startLat, startLng, destinationLat, destinationLng)
     return json.dumps(shortest_path)
    
 if __name__ == '__main__':
---
+++ src/recommender-service/service.py
@@ -0,0 +1,42 @@
+import pickle
+import os
+from math import hypot
+import networkx as nx
+
+filename = 'munich_large'
+path = os.path.dirname(__file__) + '\\map\\'
+map_nodes_serialize = path + filename + '.nodes.serialize'
+map_ways_serialize = path + filename + '.ways.serialize'
+
+node_dict = pickle.load(open(map_nodes_serialize, "rb"))
+print("Nodes Loaded")
+
+graph = pickle.load(open(map_ways_serialize, "rb"))
+print("Graph Loaded")
+
+def getNearestNode(lat, lng):
+    minDistanceNode = list(node_dict.keys())[0]
+    minDistance = hypot(node_dict[minDistanceNode][0] - lat, node_dict[minDistanceNode][1] - lng) #initialize value
+
+    # for accurate distance the haversine formula should be used however it will have little effect in our scenario
+    # a better way would be to create a search tree instead of a linear seach in the dictionary
+    for node in node_dict:
+        distance = hypot(node_dict[node][0] - lat, node_dict[node][1] - lng)
+        if distance < minDistance:
+            minDistance = distance
+            minDistanceNode = node
+    
+    return minDistanceNode
+
+def getShortestPath(startLat, startLng, endLat, endLng):
+    startNode = getNearestNode(float(startLat), float(startLng))
+    endNode = getNearestNode(float(endLat), float(endLng))
+    print(startNode, endNode)
+
+    shortest_path = nx.shortest_path(graph, source=startNode, target=endNode)
+    for index, item in enumerate(shortest_path):
+        shortest_path[index] = node_dict[item]
+
+    return shortest_path
+    
+#getShortestPath(48.133283158915276, 11.566615637573221, 48.13482978762863, 11.582279738220194)
