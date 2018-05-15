f94b7862ab4ad275391472c4a724fc3d6dac5f16
Sherjeel Sikander <sherjeel.337@gmail.com>
2018-05-15 23:31:06 +0200

    construct node dictionary from ways file

---
+++ src/recommender-service/preprocessing/node_dictionary_from_ways.py
@@ -0,0 +1,37 @@
+import sys
+import xml.etree.ElementTree as ET
+import pickle
+import time
+import os
+
+if len(sys.argv) > 2:
+    print("Invalid argument list. Only 1 filename argument allowed.")
+    sys.exit(0)
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+    path = ''
+else:
+    filename = 'munich_small'
+    path = os.path.dirname(os.path.dirname(__file__)) + '\\map\\'
+
+map_ways = path + filename + '.ways'
+map_nodes_serialize_from_ways = path + filename + '.nodes.from_ways.serialize'
+map_nodes_serialize = path + filename + '.nodes.serialize'
+node_dict_from_ways = {}
+
+try:
+    node_dict = pickle.load(open(map_nodes_serialize, "rb"))
+    print("Nodes loaded")
+    mapWaysFile = open(map_ways, 'r', encoding="utf8") 
+    for line in mapWaysFile:
+        if line.startswith('\t\t<nd ref'):
+            node = ET.fromstring(line)
+            node_dict_from_ways[node.attrib['ref']] = node_dict[node.attrib['ref']]
+
+    print("Dumping Node Dictionary")
+    pickle.dump(node_dict_from_ways, open(map_nodes_serialize_from_ways, "wb"))
+    print("Done")
+except IOError:
+    print ("Could not read file or file does not exist: ", map)
+    sys.exit()
