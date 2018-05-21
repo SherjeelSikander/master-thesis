5e0f7f9666572c647c4e0806818a7cb52f5e7437
Sherjeel Sikander <sherjeel.337@gmail.com>
2018-05-21 22:40:14 +0200

    Write test function to read edgelist

--- src/recommender-service/preprocessing/tests/read_ways_weighted_graph_test.py
+++ src/recommender-service/preprocessing/tests/read_ways_weighted_graph_test.py
@@ -3,12 +3,16 @@ import sys
 import xml.etree.ElementTree as ET
 import pickle
 import os
+import time
 
 filename = 'munich_large'
 path = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + '\\map\\'
 
 map_ways_edgelist = path + filename + '.ways.edgelist'
 
-G = nx.read_edgelist(map_ways_edgelist, nodetype=str, data=(('weight',float),('emission',float)))
-
+loadfiletime = time.time()
+graph = nx.read_edgelist(map_ways_edgelist, nodetype=str, data=(('weight',float),('emission',float)))
+graphbuildtime = time.time()
+print("Time to load file: %(time).2f sec" % {'time': (graphbuildtime - loadfiletime)})
+print("Number of edges: ", graph.number_of_edges())
 print ("done")
